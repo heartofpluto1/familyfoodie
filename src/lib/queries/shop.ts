85b354cb3c8c359d73ffe62a486dc3b93e43f71f
Sarah Spencer <sarah@chixor.net>
2025-08-10 15:11:26 +1000

    feat: add and remove items from the shopping list at will

--- src/lib/queries/shop.ts
+++ src/lib/queries/shop.ts
@@ -4,9 +4,15 @@ import { ShoppingListData, Ingredient } from '@/types/shop.js';
 export async function getIngredients() {
 	const [rows] = await pool.execute(`
       SELECT 
-        id,
-        name as ingredient__name
-      FROM menus_ingredient 
+        i.id as ingredientId,
+        i.name,
+        i.cost,
+        i.stockcode,
+        sc.name as supermarketCategory,
+        pc.name as pantryCategory
+      FROM menus_ingredient i
+      LEFT JOIN menus_supermarketcategory sc ON i.supermarketCategory_id = sc.id
+      LEFT JOIN menus_pantrycategory pc ON i.pantryCategory_id = pc.id
       WHERE public = 1
       ORDER BY name
     `);
@@ -25,6 +31,7 @@ export async function getShoppingList(week: string, year: string) {
             sl.purchased,
             sl.sort,
             ri.quantity,
+            ri.quantity4,
             m.name as quantityMeasure,
             ri.ingredient_id as ingredientId,
             sc.name as supermarketCategory,
@@ -34,7 +41,7 @@ export async function getShoppingList(week: string, year: string) {
         LEFT JOIN menus_recipeingredient ri ON sl.recipeIngredient_id = ri.id
         LEFT JOIN menus_ingredient i ON ri.ingredient_id = i.id
         LEFT JOIN menus_measure m ON ri.quantityMeasure_id = m.id
-        LEFT JOIN menus_supermarketcategory sc ON i.supermarketCategory_id = sc.id
+        LEFT JOIN menus_supermarketcategory sc ON sl.supermarketCategory_id = sc.id
         LEFT JOIN menus_pantrycategory pc ON i.pantryCategory_id = pc.id
         WHERE sl.week = ? AND sl.year = ? AND sl.fresh = 1 AND sl.account_id = 1
         ORDER BY sl.sort, sl.id;
@@ -53,6 +60,7 @@ export async function getShoppingList(week: string, year: string) {
             sl.purchased,
             sl.sort,
             ri.quantity,
+            ri.quantity4,
             m.name as quantityMeasure,
             ri.ingredient_id as ingredientId,
             sc.name as supermarketCategory,
@@ -62,7 +70,7 @@ export async function getShoppingList(week: string, year: string) {
         LEFT JOIN menus_recipeingredient ri ON sl.recipeIngredient_id = ri.id
         LEFT JOIN menus_ingredient i ON ri.ingredient_id = i.id
         LEFT JOIN menus_measure m ON ri.quantityMeasure_id = m.id
-        LEFT JOIN menus_supermarketcategory sc ON i.supermarketCategory_id = sc.id
+        LEFT JOIN menus_supermarketcategory sc ON sl.supermarketCategory_id = sc.id
         LEFT JOIN menus_pantrycategory pc ON i.pantryCategory_id = pc.id
         WHERE sl.week = ? AND sl.year = ? AND sl.fresh = 0 AND sl.account_id = 1
         ORDER BY sl.sort, sl.id;
